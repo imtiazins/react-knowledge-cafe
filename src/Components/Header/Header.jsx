51aa9192fd5fde5d934bcbdd3516eb96d8ca4cbd
“imtiazins” <imtiaz3996@gmail.com>
2023-09-24 03:09:47 +0600

    42-2 Create Fake Data for the blogs and load data

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h3 className="text-4xl font-bold text-red-500">Knowledge Cafe</h3>
+        </div>
+    );
+};
+
+export default Header;
